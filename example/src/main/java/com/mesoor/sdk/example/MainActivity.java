f3d57073932b29a2893409ba14c6d0e99d534eb1
明扬 <my@famer.me>
2019-08-23 10:07:21 +0800

    Runable

---
+++ example/src/main/java/com/mesoor/sdk/example/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mesoor.sdk.example;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import com.mesoor.zhaohu.sdk.DraggableFloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DraggableFloatingActionButton dfab = findViewById(R.id.zhaohu);
+        dfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
