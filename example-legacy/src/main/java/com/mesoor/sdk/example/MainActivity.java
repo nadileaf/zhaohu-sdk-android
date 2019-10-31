af61e31936bf4b521c48cb5bf2f66c6770842f2c
明扬 <my@famer.me>
2019-10-31 11:50:56 +0800

    不使用 androidx 的低版本兼容 example

---
+++ example-legacy/src/main/java/com/mesoor/sdk/example/MainActivity.java
@@ -0,0 +1,43 @@
+package com.mesoor.sdk.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import com.mesoor.zhaohu.sdk.DraggableFloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        EditText tokenEditText = findViewById(R.id.tokenEditText);
+        EditText fromEditText = findViewById(R.id.fromEditText);
+        RadioGroup envRadioGroup = findViewById(R.id.envRadioGroup);
+        Button initializeZhaohuButton = findViewById(R.id.initializeZhaohuButton);
+
+        DraggableFloatingActionButton zhaohu = findViewById(R.id.zhaohu);
+        zhaohu.hide();
+
+        initializeZhaohuButton.setOnClickListener(view -> {
+            zhaohu.setEnv(
+                    envRadioGroup.getCheckedRadioButtonId() == R.id.mesoorRadioButton
+                    ? DraggableFloatingActionButton.ZhaohuEnvironmentEnum.PROD
+                    : DraggableFloatingActionButton.ZhaohuEnvironmentEnum.DEV
+            );
+            zhaohu.initialize(
+                    this,
+                    tokenEditText.getText().toString(),
+                    fromEditText.getText().toString(),
+                    ZhaohuActivity.class
+            );
+            if (!zhaohu.isShown()) zhaohu.show();
+        });
+        setSupportActionBar(toolbar);
+    }
+
+}
